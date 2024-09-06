7bb29ff86f3cbc10eb1c4e35861191eba2dd10b2
Evelyn Bittelbrunn <74337638+evelynbittelbrunn@users.noreply.github.com>
2024-09-05 23:42:07 -0300

    Login Criar

--- src/hooks/useForm.jsx
+++ src/hooks/useForm.jsx
@@ -4,6 +4,10 @@ const types = {
     email: {
         regex: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
         message: 'Preencha um e-mail válido.'
+    },
+    password: {
+        regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$/,
+        message: "A senha precisa ter 1 caracter maíusculo, 1 minúsculo e 1 dígito. Com no mínimo 8 caracteres."
     }
 };
 

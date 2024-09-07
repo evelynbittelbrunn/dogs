3ddf3677ac3a8f2390bcf9dad911322f4cdb0116
Evelyn Bittelbrunn <74337638+evelynbittelbrunn@users.noreply.github.com>
2024-09-06 23:50:18 -0300

    Correção do Protected Route

--- src/components/helper/ProtectedRoute.jsx
+++ src/components/helper/ProtectedRoute.jsx
@@ -5,18 +5,17 @@ import { Navigate } from 'react-router-dom';
 const ProtectedRoute = ({ children }) => {
     const { login } = useContext(UserContext);
 
-    console.log(login)
-
     // Forma que espera para ver se o usuário realmente está logado
-    // if (login === true) {
-    //     return children;
-    // } else if (login === false) {
-    //     return <Navigate to="/login" />
-    // } else {
-    //     return <></>
-    // }
+    if (login === true) {
+        return children;
+    } else if (login === false) {
+        return <Navigate to="/login" />
+    } else {
+        return <></>
+    }
 
-    return login ? children : <Navigate to="/login" />;
+    // Forma que apenas manda o usuário para a página de login
+    // return login ? children : <Navigate to="/login" />;
 }
 
 export default ProtectedRoute

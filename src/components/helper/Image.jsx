bd8e9040736e7b7d2428a3b4a88d1a77ab7bd204
Evelyn Bittelbrunn <74337638+evelynbittelbrunn@users.noreply.github.com>
2024-09-08 10:19:02 -0300

    Image Skeleton
    Criação de um componente para a imagem que mostre um skeleton quando a imagem ainda estiver carregando.

---
+++ src/components/helper/Image.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+import styles from './Image.module.css';
+
+const Image = ({ alt, ...props }) => {
+
+    const [skeleton, setSkeleton] = useState(true);
+
+    // Função só ocorre quando a imagem é carregada 100% devido ao evento onLoad do img
+    function handleLoad({ target }) {
+        setSkeleton(false);
+        target.style.opacity = 1;
+    }
+
+    return (
+        <div className={styles.wrapper}>
+            {skeleton && <div className={styles.skeleton}></div>}
+            <img onLoad={handleLoad} className={styles.img} alt={alt} {...props} />
+        </div>
+    )
+}
+
+export default Image
--- src/components/photo/PhotoContent.jsx
+++ src/components/photo/PhotoContent.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import PhotoComments from './PhotoComments';
 import { UserContext } from '../../contexts/UserContext';
 import PhotoDelete from './PhotoDelete';
+import Image from '../helper/Image';
 
 const PhotoContent = ({ data }) => {
 
@@ -13,7 +14,7 @@ const PhotoContent = ({ data }) => {
     return (
         <div className={styles.photo}>
             <div className={styles.img}>
-                <img src={photo.src} alt={photo.title} />
+                <Image src={photo.src} alt={photo.title} />
             </div>
             <div className={styles.details}>
                 <div>

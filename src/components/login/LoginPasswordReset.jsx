a4a6b4049155f4e9f0ecf50d18c28de6c2a533b5
Evelyn Bittelbrunn <74337638+evelynbittelbrunn@users.noreply.github.com>
2024-09-10 00:35:49 -0300

    Perdeu Senha - Parte 2
    Feita a parte de resetar (trocar) a senha caso o usuário tenha clicado no link de redefinição enviado no e-mail.

--- src/components/login/LoginPasswordReset.jsx
+++ src/components/login/LoginPasswordReset.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom';
+import Input from '../forms/input/Input';
+import Button from '../forms/button/Button';
+import useForm from '../../hooks/useForm';
+import { PASSWORD_RESET } from '../../api';
+import useFetch from '../../hooks/useFetch';
+import Error from '../error/Error';
 
 const LoginPasswordReset = () => {
+
+    const [login, setLogin] = useState('');
+    const [key, setKey] = useState('');
+    const password = useForm('password');
+    const { error, loading, request } = useFetch();
+
+    const navigate = useNavigate();
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+        if (password.validate()) {
+            const { url, options } = PASSWORD_RESET({
+                login, key, password: password.value
+            });
+            const { response } = await request(url, options);
+            if (response.ok) navigate('/login');
+        }
+    }
+
+    useEffect(() => {
+        const params = new URLSearchParams(window.location.search);
+        const key = params.get('key');
+        const login = params.get('login');
+        if (key) setKey(key);
+        if (login) setLogin(login);
+    }, []);
+
     return (
-        <div>LoginPasswordReset</div>
+        <div>
+            <h1 className='title'>Resete a Senha</h1>
+            <form onSubmit={handleSubmit}>
+                <Input label="Nova Senha" type="password" name="password" {...password} />
+                {loading ? <Button disabled>Resetando...</Button> : <Button>Resetar</Button>}
+                <Error error={error} />
+            </form>
+        </div>
     )
 }
 

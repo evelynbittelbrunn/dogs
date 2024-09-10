3d169637bb77a9de8108660890772a8b4991cff0
Evelyn Bittelbrunn <74337638+evelynbittelbrunn@users.noreply.github.com>
2024-09-10 00:17:17 -0300

    Perdeu Senha - Parte 2
    Desenvolvida a tela de "Perdeu a senha?" que envia o e-mail gerando o link para o usuário resetar. Nesse commit tem apenas a primeira tela, a tela de resetar será no próximo commit.

--- src/components/login/LoginPasswordLost.jsx
+++ src/components/login/LoginPasswordLost.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import Input from '../forms/input/Input'
+import Button from '../forms/button/Button'
+import useForm from '../../hooks/useForm'
+import useFetch from '../../hooks/useFetch'
+import { PASSWORD_LOST } from '../../api'
+import Error from '../error/Error'
 
 const LoginPasswordLost = () => {
+
+    const login = useForm();
+    const { data, loading, error, request } = useFetch();
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+        if (login.validate()) {
+            const { url, options } = PASSWORD_LOST({
+                login: login.value,
+                url: window.location.href.replace('perdeu', 'resetar'),
+            });
+            request(url, options);
+        }
+    }
+
     return (
-        <div>LoginPasswordLost</div>
+        <section>
+            <h1 className='title'>Perdeu a senha?</h1>
+            {data ? (
+                <p style={{ color: '#4c1' }}>{data}</p>
+            ) : (
+                <form onSubmit={handleSubmit}>
+                    <Input label="Email / Usuário" type="text" name="login" {...login} />
+                    {loading ? <Button disabled>Enviando...</Button> : <Button>Enviar E-mail</Button>}
+                </form>
+            )}
+            <Error error={error} />
+        </section>
     )
 }
 

72c8d9fd80fab8d8db841eb4903bb4305d0d5ddd
Evelyn Bittelbrunn <74337638+evelynbittelbrunn@users.noreply.github.com>
2024-09-10 01:03:39 -0300

    Footer
    Footer estilizado e feito para que fique fixo sempre na parte inferior da tela.

--- src/App.jsx
+++ src/App.jsx
@@ -13,18 +13,20 @@ import UserProfile from './components/user/UserProfile';
 import NotFound from './components/helper/NotFound';
 
 const App = () => {
-    return <div>
+    return <div className='App'>
         <BrowserRouter>
             <UserStorage>
                 <Header />
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="login/*" element={<Login />} />
-                    <Route path="conta/*" element={<ProtectedRoute><User /></ProtectedRoute>} />
-                    <Route path="foto/:id" element={<Photo />} />
-                    <Route path="perfil/:user" element={<UserProfile />} />
-                    <Route path="*" element={<NotFound />} />
-                </Routes>
+                <main className='AppBody'>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="login/*" element={<Login />} />
+                        <Route path="conta/*" element={<ProtectedRoute><User /></ProtectedRoute>} />
+                        <Route path="foto/:id" element={<Photo />} />
+                        <Route path="perfil/:user" element={<UserProfile />} />
+                        <Route path="*" element={<NotFound />} />
+                    </Routes>
+                </main>
                 <Footer />
             </UserStorage>
         </BrowserRouter>
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Dogs from '../../assets/dogs.svg?react';
 import styles from './Footer.module.css'
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <footer className={styles.footer}>
+            <Dogs />
+            <p>Dogs. Alguns direitos reservados.</p>
+        </footer>
     )
 }
 
